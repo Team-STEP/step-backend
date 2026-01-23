0a7e7099fbb885ae2b055838dd3b61efb5525da8
cpdmpara <leebadagyumin0129@gmail.com>
2026-01-23 21:29:20 +0900

    build: initial commit

---
+++ src/main/java/com/teamstep/stepbackend/StepBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamstep.stepbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StepBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamstep/stepbackend/StepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamstep.stepbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
