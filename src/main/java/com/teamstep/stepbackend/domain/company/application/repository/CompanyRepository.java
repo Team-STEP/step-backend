466e85caadc3904056d18522dcedf370cdd9ebb2
rulrularla123 <kimdh3700@gmail.com>
2026-01-28 00:16:57 +0900

    fix: 제네릭 없던거 추가

--- src/main/java/com/teamstep/stepbackend/domain/company/application/repository/CompanyRepository.java
+++ src/main/java/com/teamstep/stepbackend/domain/company/application/repository/CompanyRepository.java
@@ -1,9 +1,10 @@
 package com.teamstep.stepbackend.domain.company.application.repository;
 
+import com.teamstep.stepbackend.domain.company.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompanyRepository extends JpaRepository {
+public interface CompanyRepository extends JpaRepository<Company, String> {
 
 }
