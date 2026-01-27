9a5ce3ba93bba8f25655a1fec9a55f6041762555
eunchong <eunchong@eunchongs-MacBook-Air.local>
2026-01-27 21:17:05 +0900

    feat: usecase

--- src/main/java/com/teamstep/stepbackend/domain/company/adapter/CompanyController.java
+++ src/main/java/com/teamstep/stepbackend/domain/company/adapter/CompanyController.java
@@ -1,4 +1,7 @@
 package com.teamstep.stepbackend.domain.company.adapter;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CompanyController{
 }
---
+++ src/main/java/com/teamstep/stepbackend/domain/company/application/CompanyUseCase.java
@@ -0,0 +1,7 @@
+package com.teamstep.stepbackend.domain.company.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompanyUseCase {
+}
