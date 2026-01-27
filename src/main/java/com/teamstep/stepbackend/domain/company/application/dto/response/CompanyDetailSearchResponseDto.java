e4c39a689f2b335764d2c98f86c7aa6e22f028a6
eunchong <eunchong@eunchongs-MacBook-Air.local>
2026-01-27 23:01:01 +0900

    feat: search dto

---
+++ src/main/java/com/teamstep/stepbackend/domain/company/application/dto/response/CompanyDetailSearchResponseDto.java
@@ -0,0 +1,51 @@
+package com.teamstep.stepbackend.domain.company.application.dto.response;
+
+import com.teamstep.stepbackend.domain.company.entity.Company;
+
+public record CompanyDetailSearchResponseDto(
+        String id,
+        String businesspersonCode,
+        String location,
+        String representativeName,
+        String foundationDate,
+        String type,
+        String area,
+        String description,
+        String websiteUrl,
+        String capital,
+        String annualSales,
+        Long maleEmployee,
+        Long femaleEmployee,
+        String managerDepartment,
+        String managerPosition,
+        String managerName,
+        String managerFax,
+        String managerPhoneNumber,
+        String managerCellPhoneNumber,
+        String managerEmail
+) {
+    public static CompanyDetailSearchResponseDto from(Company company) {
+        return new CompanyDetailSearchResponseDto(
+                company.getId(),
+                company.getBusinesspersonCode(),
+                company.getLocation(),
+                company.getRepresentativeName(),
+                company.getFoundationDate(),
+                company.getType(),
+                company.getArea(),
+                company.getDescription(),
+                company.getWebsiteUrl(),
+                company.getCapital(),
+                company.getAnnualSales(),
+                company.getMaleEmployee(),
+                company.getFemaleEmployee(),
+                company.getManagerDepartment(),
+                company.getManagerPosition(),
+                company.getManagerName(),
+                company.getManagerFax(),
+                company.getManagerPhoneNumber(),
+                company.getManagerCellPhoneNumber(),
+                company.getManagerEmail()
+                );
+    }
+}
