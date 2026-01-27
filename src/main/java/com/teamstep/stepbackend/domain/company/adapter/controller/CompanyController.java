e4641a5d5aab5575229caaab99c46193bd4df364
rulrularla123 <kimdh3700@gmail.com>
2026-01-28 04:40:00 +0900

    feat: create, update 구현

--- src/main/java/com/teamstep/stepbackend/domain/company/adapter/controller/CompanyController.java
+++ src/main/java/com/teamstep/stepbackend/domain/company/adapter/controller/CompanyController.java
@@ -1,15 +1,40 @@
 package com.teamstep.stepbackend.domain.company.adapter.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.teamstep.stepbackend.domain.company.application.dto.request.CompanyCreateRequestDto;
+import com.teamstep.stepbackend.domain.company.application.dto.request.CompanyUpdateRequestDto;
+import com.teamstep.stepbackend.domain.company.application.dto.response.CompanyCreateResponseDto;
+import com.teamstep.stepbackend.domain.company.application.usecase.CreateCompanyUseCase;
+import com.teamstep.stepbackend.domain.company.application.usecase.UpdateCompanyUseCase;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 // PreAuthorize
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/companies")
 public class CompanyController{
+
+    private final CreateCompanyUseCase createCompanyUseCase;
+    private final UpdateCompanyUseCase updateCompanyUseCase;
+
     // Write
-    // Create
-    // Update
+    @PostMapping("/companies")
+    public ResponseEntity<CompanyCreateResponseDto> createCompany(
+            @Valid @RequestBody CompanyCreateRequestDto requestDto
+    ) {
+        CompanyCreateResponseDto response = createCompanyUseCase.createCompany(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/companies/{company-id}")
+    public void updateCompany(
+            @Valid @RequestParam String id, @RequestBody CompanyUpdateRequestDto requestDto
+    ){
+        updateCompanyUseCase.updateCompany(id, requestDto);
+    }
 
     // Read
     // ListSearch
