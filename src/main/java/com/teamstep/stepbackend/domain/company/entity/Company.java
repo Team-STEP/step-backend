7c2a7a44f96a784af83e6ba5d320b2a4854efebf
rulrularla123 <kimdh3700@gmail.com>
2026-01-27 17:04:18 +0900

    style: 엔티티 변경
    Added new fields for company type, area, description, capital, annual sales, employee counts, and manager details.

--- src/main/java/com/teamstep/stepbackend/domain/company/entity/Company.java
+++ src/main/java/com/teamstep/stepbackend/domain/company/entity/Company.java
@@ -13,4 +13,20 @@ public class Company {
     private String representativeName;
     private String foundationDate;
     private String websiteUrl;
+    private String type;
+    private String area;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String capital;
+    private String annualSales;
+    private Long totalEmployee;
+    private Long maleEmployee;
+    private Long femaleEmployee;
+    private String managerDepartment;
+    private String managerPosition;
+    private String managerName;
+    private String managerFax;
+    private String managerPhoneNumber;
+    private String managerCellPhoneNumber;
+    private String managerEmail;
 }
