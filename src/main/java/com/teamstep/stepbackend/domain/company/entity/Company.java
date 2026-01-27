a1cd43e438f7703d88a17663560a564052a9ce4c
rulrularla123 <kimdh3700@gmail.com>
2026-01-27 17:33:09 +0900

    style: company 엔티티 변경

--- src/main/java/com/teamstep/stepbackend/domain/company/entity/Company.java
+++ src/main/java/com/teamstep/stepbackend/domain/company/entity/Company.java
@@ -1,5 +1,6 @@
 package com.teamstep.stepbackend.domain.company.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,4 +14,20 @@ public class Company {
     private String representativeName;
     private String foundationDate;
     private String websiteUrl;
+    private String type;
+    private String area;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String capital;
+    private String annualSales;
+    private Long totalEmployee;
+    private Long maleEmployee;
+    private Long femaleEmployee;
+    private String managerDepartment;
+    private String managerPosition;
+    private String managerName;
+    private String managerFax;
+    private String managerPhoneNumber;
+    private String managerCellPhoneNumber;
+    private String managerEmail;
 }
