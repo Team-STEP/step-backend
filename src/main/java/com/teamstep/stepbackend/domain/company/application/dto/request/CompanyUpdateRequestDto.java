6206c9fb0911db2d7f349e5bd96f5c6d320944a2
rulrularla123 <kimdh3700@gmail.com>
2026-01-27 23:28:17 +0900

    fix: 오타수정

--- src/main/java/com/teamstep/stepbackend/domain/company/application/dto/request/CompanyUpdateRequestDto.java
+++ src/main/java/com/teamstep/stepbackend/domain/company/application/dto/request/CompanyUpdateRequestDto.java
@@ -1,9 +1,9 @@
 package com.teamstep.stepbackend.domain.company.application.dto.request;
 
-import com.teamstep.stepbackend.domain.company.application.dto.response.CompanySearchReponseDto;
+import com.teamstep.stepbackend.domain.company.application.dto.response.CompanySearchResponseDto;
 
 import java.util.List;
 
 public record CompanyUpdateRequestDto(
-        List<CompanySearchReponseDto> companies
+        List<CompanySearchResponseDto> companies
 ) {}
