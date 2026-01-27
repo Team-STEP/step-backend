86101e7a53a11a7ebdea73f0451f3caa76c7d98d
rulrularla123 <kimdh3700@gmail.com>
2026-01-27 23:47:43 +0900

    feat: Entity변환코드 추가

--- src/main/java/com/teamstep/stepbackend/domain/company/application/dto/request/CompanyUpdateRequestDto.java
+++ src/main/java/com/teamstep/stepbackend/domain/company/application/dto/request/CompanyUpdateRequestDto.java
@@ -1,9 +1,56 @@
 package com.teamstep.stepbackend.domain.company.application.dto.request;
 
 import com.teamstep.stepbackend.domain.company.application.dto.response.CompanySearchResponseDto;
+import com.teamstep.stepbackend.domain.company.entity.Company;
 
 import java.util.List;
 
 public record CompanyUpdateRequestDto(
-        List<CompanySearchResponseDto> companies
-) {}
+        String id,
+        String companyName,
+        String businesspersonCode,
+        String location,
+        String representativeName,
+        String foundationDate,
+        String websiteUrl,
+        String type,
+        String area,
+        String description,
+        String capital,
+        String annualSales,
+        Long maleEmployee,
+        Long femaleEmployee,
+        String managerDepartment,
+        String managerPosition,
+        String managerName,
+        String managerFax,
+        String managerPhoneNumber,
+        String managerCellPhoneNumber,
+        String managerEmail
+) {
+    public Company toEntity(){
+        return Company.builder()
+                .id(this.id)
+                .companyName(this.companyName)
+                .businesspersonCode(this.businesspersonCode)
+                .location(this.location)
+                .representativeName(this.representativeName)
+                .foundationDate(this.foundationDate)
+                .websiteUrl(this.websiteUrl)
+                .type(this.type)
+                .area(this.area)
+                .description(this.description)
+                .capital(this.capital)
+                .annualSales(this.annualSales)
+                .maleEmployee(this.maleEmployee)
+                .femaleEmployee(this.femaleEmployee)
+                .managerDepartment(this.managerDepartment)
+                .managerPosition(this.managerPosition)
+                .managerName(this.managerName)
+                .managerFax(this.managerFax)
+                .managerPhoneNumber(this.managerPhoneNumber)
+                .managerCellPhoneNumber(this.managerCellPhoneNumber)
+                .managerEmail(this.managerEmail)
+                .build();
+    }
+}
