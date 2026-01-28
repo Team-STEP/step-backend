bd0a51e2504d2780e2c121549935a07ff2c2a563
rulrularla123 <kimdh3700@gmail.com>
2026-01-28 10:52:23 +0900

    refactor: companyId 변수명 변경

--- src/main/java/com/teamstep/stepbackend/domain/company/application/dto/response/CompanyCreateResponseDto.java
+++ src/main/java/com/teamstep/stepbackend/domain/company/application/dto/response/CompanyCreateResponseDto.java
@@ -3,11 +3,11 @@ package com.teamstep.stepbackend.domain.company.application.dto.response;
 import com.teamstep.stepbackend.domain.company.entity.Company;
 
 public record CompanyCreateResponseDto(
-        String id
+        String companyId
 ) {
     public static CompanyCreateResponseDto from(Company company) {
         return new CompanyCreateResponseDto(
-                company.getId()
+                company.getCompanyId()
         );
     }
 }
