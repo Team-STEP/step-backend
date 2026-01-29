bb9e5a336ad076799592f65a4291669211555db6
eunchong <eunchong@eunchongs-MacBook-Air.local>
2026-01-29 12:59:14 +0900

    fix: Search Dto

--- src/main/java/com/teamstep/stepbackend/domain/recruitment/application/dto/response/RecruitmentSearchResponseDto.java
+++ src/main/java/com/teamstep/stepbackend/domain/recruitment/application/dto/response/RecruitmentSearchResponseDto.java
@@ -1,26 +1,24 @@
 package com.teamstep.stepbackend.domain.recruitment.application.dto.response;
 
-import com.teamstep.stepbackend.domain.company.entity.Company;
+import com.teamstep.stepbackend.domain.company.application.dto.response.CompanySummaryDto;
 import com.teamstep.stepbackend.domain.recruitment.entity.Recruitment;
 
 import java.util.List;
 
 public record RecruitmentSearchResponseDto(
         String recruitmentId,
-        Long recruitmentCount,
         String jobDescription,
         String major,
         String jobType,
-        List<CompanySummaryDto> companies
+        CompanySummaryDto company
 ) {
-    public static RecruitmentSearchResponseDto of(Long recruitmentCount, Recruitment recruitment, List<CompanySummaryDto> companyList){
+    public static RecruitmentSearchResponseDto from(Recruitment recruitment){
         return new RecruitmentSearchResponseDto(
                 recruitment.getRecruitmentId(),
-                recruitmentCount,
                 recruitment.getJobDescription(),
                 recruitment.getMajor(),
                 recruitment.getJobType(),
-                companyList
+                CompanySummaryDto.from(recruitment.getCompany())
         );
     }
 }
