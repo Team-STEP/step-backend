f9ba25b6f69e7f02e083f42811373a69d3e414e9
eunchong <eunchong@eunchongs-MacBook-Air.local>
2026-01-29 13:01:05 +0900

    feat: Search Mopping

--- src/main/java/com/teamstep/stepbackend/domain/company/adapter/controller/CompanyController.java
+++ src/main/java/com/teamstep/stepbackend/domain/company/adapter/controller/CompanyController.java
@@ -1,8 +1,16 @@
 package com.teamstep.stepbackend.domain.company.adapter.controller;
 
-import com.teamstep.stepbackend.domain.company.application.dto.request.*;
-import com.teamstep.stepbackend.domain.company.application.dto.response.*;
-import com.teamstep.stepbackend.domain.company.application.usecase.*;
+import com.teamstep.stepbackend.domain.company.application.dto.request.CompanyCreateRequestDto;
+import com.teamstep.stepbackend.domain.company.application.dto.request.CompanyDetailSearchRequestDto;
+import com.teamstep.stepbackend.domain.company.application.dto.request.CompanySearchRequestDto;
+import com.teamstep.stepbackend.domain.company.application.dto.request.CompanyUpdateRequestDto;
+import com.teamstep.stepbackend.domain.company.application.dto.response.CompanyCreateResponseDto;
+import com.teamstep.stepbackend.domain.company.application.dto.response.CompanyDetailSearchResponseDto;
+import com.teamstep.stepbackend.domain.company.application.dto.response.CompanyListSearchResponseDto;
+import com.teamstep.stepbackend.domain.company.application.usecase.CreateCompanyUseCase;
+import com.teamstep.stepbackend.domain.company.application.usecase.GetCompanyListUseCase;
+import com.teamstep.stepbackend.domain.company.application.usecase.GetCompanyUseCase;
+import com.teamstep.stepbackend.domain.company.application.usecase.UpdateCompanyUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -21,7 +29,6 @@ public class CompanyController{
     private final GetCompanyListUseCase getCompanyListUseCase;
     private final GetCompanyUseCase getCompanyUseCase;
 
-    // Write
     // Create
     @PostMapping
     public ResponseEntity<CompanyCreateResponseDto> createCompany(
@@ -32,29 +39,29 @@ public class CompanyController{
     }
 
     // Update
-    @PostMapping("/{id}")
+    @PutMapping("/{companyId}")
     public ResponseEntity<Void> updateCompany(
-            @Valid @PathVariable("id") String id, @RequestBody CompanyUpdateRequestDto requestDto
+            @PathVariable String companyId, @Valid @RequestBody CompanyUpdateRequestDto requestDto
     ) {
-        updateCompanyUseCase.updateCompany(id, requestDto);
+        updateCompanyUseCase.updateCompany(companyId, requestDto);
         return ResponseEntity.ok().build();
     }
 
-    // Read
     // ListSearch
     @GetMapping
-    public ResponseEntity<CompanyListSearchResposneDto> getCompanies(
-            @ModelAttribute CompanySearchRequestDto filter,
+    public ResponseEntity<CompanyListSearchResponseDto> getCompanyList(
+            @ModelAttribute CompanySearchRequestDto requestDto,
             @PageableDefault(size = 20) Pageable pageable
     ) {
-        return ResponseEntity.ok(getCompanyListUseCase.getCompanyList(filter, pageable));
+        return ResponseEntity.ok(getCompanyListUseCase.getCompanyList(requestDto, pageable));
     }
 
     // DetailSearch
     @GetMapping("/{companyId}")
-    public ResponseEntity<CompanyDetailSearchResponseDto> getCompany(
-            @PathVariable("companyId") String companyId
+    public ResponseEntity<CompanyDetailSearchResponseDto> getCompanyDetail(
+            @PathVariable String companyId
     ) {
-        return ResponseEntity.ok(getCompanyUseCase.getCompanyById(companyId));
+        CompanyDetailSearchRequestDto companyDetailSearchRequestDto = new CompanyDetailSearchRequestDto(companyId);
+        return ResponseEntity.ok(getCompanyUseCase.getCompanyById(companyDetailSearchRequestDto));
     }
 }
